bbc4b9af6327d286a7fa2b77d9017aa543969fe0
Emmanuel Onuegbu <emma.onuegbu11@gmail.com>
2024-03-04 12:29:38 +0100

    new commit

--- cypress/e2e/clickUpTest.cy.js
+++ cypress/e2e/clickUpTest.cy.js
@@ -5,6 +5,8 @@ describe("This test is to validate Task scenarios on the Clickup web app", () =>
       cy.visit("https://app.clickup.com/login");
       cy.get('[data-test="login-email-input"]').type("emma.onuegbu11@gmail.com");
       cy.get('[data-test="login-password-input"]').type("Emenike1965{Enter}");
+      cy.get('[data-test="avatar"]').should("be.visible");
+
 
 
       
